e5950815467d2cabeec6533fe57022dc0dc8e1c4
toshi-toma <toshitoma1217@gmail.com>
2019-03-30 16:28:51 +0900

    change entory file extension

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: path.resolve(__dirname, 'src/index.jsx'),
 
   output: {
     path: path.resolve(__dirname, 'public'),
